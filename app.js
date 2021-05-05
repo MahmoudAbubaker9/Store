cf84ca51bb7a6d1aa342ad70f3d9c6af4030a47a
MahmoudAbubaker <>
2021-05-05 13:04:33 +0000

    addjava

---
+++ app.js
@@ -0,0 +1,14 @@
+var userName = prompt('whats your name?')
+
+alert('Hello ' + userName + ' nice to meet you')
+
+var phone = prompt('Whats your favourite Phone Samsung or Iphone??')
+
+if(phone == 'samsung'){
+  document.write('<img src="https://www.freepnglogos.com/uploads/smartphone-png/smartphone-icon-png-mobile-photo-phone-image-mobile-frame-27.png">')
+}else if(phone == 'iphone'){
+  document.write('<img src="https://media.voltron.alhurra.com/Drupal/01live-106/styles/1984x1200/s3/2019-12/BA7B6A3D-DEB7-4794-A7B7-F0A17541E302.jpg?itok=32Rt8bf7">')
+}  
+
+let result = confirm('i hoepe that you like the store')
+console.log(result)
