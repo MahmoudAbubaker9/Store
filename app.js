4729ca4186f272ed6e0defb7e3895fab9d4de114
MahmoudAbubaker <>
2021-05-05 13:15:26 +0000

    finl edit

--- app.js
+++ app.js
@@ -10,16 +10,4 @@ if(phone == 'samsung'){
   document.write('<img src="https://media.voltron.alhurra.com/Drupal/01live-106/styles/1984x1200/s3/2019-12/BA7B6A3D-DEB7-4794-A7B7-F0A17541E302.jpg?itok=32Rt8bf7">')
 }  
 
-let result = confirm('i hoepe that you like the store')
-console.log(result)
-
-let result1 = confirm('Did you like the store?')
-console.log(result1)
-
-var result2 = prompt('Can you give us a Rate out of 10')
-console.log(result2)
-
-//the result1 and result2 showen in console
-
-
->>>>>>> cc6fc5462a64063c3fac90f28bd392a25b6af528
+let resultm = confirm('i hoepe that you like the store')
