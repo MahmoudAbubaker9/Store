cc6fc5462a64063c3fac90f28bd392a25b6af528
MahmoudAbubaker <>
2021-05-05 11:23:00 +0000

    Java

---
+++ app.js
@@ -0,0 +1,14 @@
+var userName = prompt('whats your name?')
+
+alert('Hello ' + userName + ' nice to meet you')
+
+
+let result1 = confirm('Did you like the store?')
+console.log(result1)
+
+var result2 = prompt('Can you give us a Rate out of 10')
+console.log(result2)
+
+//the result1 and result2 showen in console
+
+
