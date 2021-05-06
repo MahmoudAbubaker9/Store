a4e0575ceb0b1b9bc966829d1e40dba14eeacaa2
MahmoudAbubaker <>
2021-05-06 11:11:36 +0000

    edit

--- app.js
+++ app.js
@@ -2,24 +2,28 @@ var userName = prompt('whats your name?')
 
 alert('Hello ' + userName + ' nice to meet you')
 
-var phone = prompt('Whats your favourite Phone Samsung or Iphone??')
+var pass = prompt('did we offer HUAWEI phone. please answer with yes or NO')
+var correct = 'no'
 
-if(phone == 'samsung'){
-  document.write('<img src="https://www.freepnglogos.com/uploads/smartphone-png/smartphone-icon-png-mobile-photo-phone-image-mobile-frame-27.png">')
-}else if(phone == 'iphone'){
-  document.write('<img src="https://media.voltron.alhurra.com/Drupal/01live-106/styles/1984x1200/s3/2019-12/BA7B6A3D-DEB7-4794-A7B7-F0A17541E302.jpg?itok=32Rt8bf7">')
-}  
+while(pass !== correct ) {
+pass = prompt('Wrong!! , try agin')
+}
 
-let result = confirm('i hoepe that you like the store')
-console.log(result)
+var phone = prompt('Whats your favourite Phone samsung or iphone??') 
 
-let result1 = confirm('Did you like the store?')
-console.log(result1)
+while(phone !== 'iphone' && phone !== 'samsung'){
+  phone = prompt('Please choose from the available options ')
+}
+var counter = prompt('How many Phone do you want to see ?')
 
-var result2 = prompt('Can you give us a Rate out of 10')
-console.log(result2)
+for( var y=1 ; y <= counter ; y++ ){
+  if(phone === 'iphone'){
+    document.write('<img src="https://www.blitzmicro.eu/11091-medium_default/AP-MGMP3QLA.jpg">'+ y )
+  }else if(phone === 'samsung'){
+    document.write('<img src="https://quickfixcentre.com/wp-content/uploads/2020/09/image.jpg">'+ y)
+  }
 
-//the result1 and result2 showen in console
 
+}
 
->>>>>>> cc6fc5462a64063c3fac90f28bd392a25b6af528
+let resultm = confirm('i hoepe that you like the store')
